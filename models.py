a6f00f399398c4129d052f45d72e4c60d35b545e
Ian Livingston <i.livingston.c@gmail.com>
2019-11-25 15:33:15 -0700

    discard these changes

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ if 'ON_HEROKU' in os.environ:
 else:
     DATABASE = PostgresqlExtDatabase(
         'car_app',
-        user='home',
+        user='ian',
         password='password',
         host='localhost'
     )
